7ded9d77a78c93b2947a7ca2cdbc03c932b6cd32
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-04 16:15:53 +0100

    Remove default code & create game class in f#

---
+++ HybridRogue.Game/Game1.fs
@@ -0,0 +1,26 @@
+﻿namespace HybridRogue.Game
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type Game1() as this = 
+    inherit Microsoft.Xna.Framework.Game()
+    do this.Content.RootDirectory <- "Content"
+    do this.Window.Title <- "Hybrid Rogue"
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+    let graphics = new GraphicsDeviceManager(this)
+    do graphics.PreferredBackBufferHeight <- 600
+    do graphics.PreferredBackBufferWidth <- 800
+    let device = graphics.GraphicsDevice
+
+    override this.Initialize() =
+        ()
+
+    override this.LoadContent() =
+        do spriteBatch <- new SpriteBatch(device)
+    
+    override this.Update(time: GameTime) =
+        ()
+    
+    override this.Draw(time: GameTime) =
+        device.Clear Color.CornflowerBlue
