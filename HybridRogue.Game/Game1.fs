4021dd02c5f6483724493205aa6d008b1ace4bce
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-04 21:23:34 +0100

    Add game state record

--- HybridRogue.Game/Game1.fs
+++ HybridRogue.Game/Game1.fs
@@ -2,24 +2,27 @@
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open State
 
 type Game1() as this = 
     inherit Microsoft.Xna.Framework.Game()
     do this.Content.RootDirectory <- "Content"
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
     let graphics = new GraphicsDeviceManager(this)
+    let mutable state = Unchecked.defaultof<GameState>
 
     override this.Initialize() =
         do this.Window.Title <- "Hybrid Rogue"
         do graphics.PreferredBackBufferHeight <- 600
         do graphics.PreferredBackBufferWidth <- 800
         do graphics.ApplyChanges ()
+        do state <- initialGameState
 
     override this.LoadContent() =
         do spriteBatch <- new SpriteBatch(graphics.GraphicsDevice)
     
     override this.Update(time: GameTime) =
-        ()
+        do state <- updateState state
     
     override this.Draw(time: GameTime) =
         graphics.GraphicsDevice.Clear Color.CornflowerBlue
---
+++ HybridRogue.Game/State.fs
@@ -0,0 +1,8 @@
+﻿module HybridRogue.Game.State
+
+type GameState = { level: int }
+
+let initialGameState = { level = 1 }
+
+let updateState (state: GameState) =
+    state
