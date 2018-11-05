09d1ef23605e430e94d5e056e088cb037cc5286b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-05 20:25:12 +0100

    Simple glyph drawing (uncentered)

--- FontGenerator/Generator.fs
+++ FontGenerator/Generator.fs
@@ -5,6 +5,9 @@ open System.IO
 open System.Collections.Generic
 open SixLabors.Primitives
 open SixLabors.Fonts
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+open SixLabors.ImageSharp.Processing
 
 type GeneratorSettings = {
     fontFileName: string;
@@ -20,8 +23,9 @@ let upperChar = 127
 
 let generate (settings: GeneratorSettings): Result<string, string> =
     let fontCollection = FontCollection()
-    let font = fontCollection.Install(File.OpenRead(settings.fontFileName))
-    
+    let fontFamily = fontCollection.Install(File.OpenRead(settings.fontFileName))
+    let font = fontFamily.CreateFont(float32(settings.fontSize))
+
     let mutable bitmapWidth = 0
     let mutable bitmapHeight = 0
 
@@ -30,11 +34,20 @@ let generate (settings: GeneratorSettings): Result<string, string> =
     for i = 33 to 127 do
         let newWidth = bitmapWidth + 64
         let character = char(i)
-        glyphs.Add(character, Point(bitmapWidth, bitmapHeight))
+        glyphs.Add(character, PointF(float32(bitmapWidth), float32(bitmapHeight)))
         if newWidth > bitmapMaxWidth then
             bitmapWidth <- 64
             bitmapHeight <- bitmapHeight + 64
         else
-            bitmapWidth <- newWidth    
+            bitmapWidth <- newWidth
+            
+    let image = new Image<Rgba32>(bitmapWidth, bitmapHeight)
+
+    for entry in glyphs do
+        let character = entry.Key
+        let pos = entry.Value
+        do image.Mutate(fun ctx -> ctx.DrawText(string(character), font, Rgba32.White, pos) |> ignore)
+
+    image.Save(settings.outFileName)
 
     Error("Not Implemented")
