27b780ba7658e01ce189447a0d6a4da2b785b989
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-05 15:23:56 +0100

    Parse command line args

--- FontGenerator/Program.fs
+++ FontGenerator/Program.fs
@@ -1,8 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open CommandLine
+open CommandLine.Text
+
+type CommandLineOptions = {
+    [<Value(0, MetaName = "font", HelpText = "Font file")>] fontFile: string;
+    [<Option("size", HelpText = "Font size", Default = 64)>] fontSize: int;
+    [<Option("out", Required = true, HelpText = "Image file output name")>] outputFileName: string;
+    [<Option("bg", Default = "(0,0,0)", HelpText = "Background Color")>] backgroundColor: string;
+    [<Option("fg", Default = "(255, 255, 255)", HelpText = "Foreground Color")>] foregroundColor: string
+}
 
 [<EntryPoint>]
 let main argv =
-    
-    0
+    let result = CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(argv)
+    match result with
+        | :? Parsed<CommandLineOptions> as parsed -> 
+            
+            0
+        | :? NotParsed<CommandLineOptions> as notParsed -> 1
+        | _ -> 2
