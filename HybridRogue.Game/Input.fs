7e558df2efe29eca0dab64b1261fd882451a68fe
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-07 15:53:33 +0100

    Add types for input handling

---
+++ HybridRogue.Game/Input.fs
@@ -0,0 +1,20 @@
+﻿module HybridRogue.Game.Input
+
+open Microsoft.Xna.Framework.Input
+open Microsoft.Xna.Framework
+
+type MouseButton = Left | Middle | Right
+
+type TextEvent = { character: char option; key: Keys }
+
+type InputEvent = 
+        | Down of  MouseButton * Vector2
+        | Up of MouseButton * Vector2
+        | Scrolled of int
+        | Pressed of Keys
+        | Released of Keys
+        | TextInput of TextEvent
+
+type InputState = { lastPressedKeys: Keys array; lastMouseState: MouseState }
+
+let emptyInputState = { lastPressedKeys = [||]; lastMouseState = MouseState() }
