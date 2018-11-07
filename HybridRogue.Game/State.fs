a874379c3c1745cb88603fa78dfa26ca84708259
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-07 15:38:20 +0100

    Add dummy menu

--- HybridRogue.Game/State.fs
+++ HybridRogue.Game/State.fs
@@ -2,6 +2,7 @@
 
 open HybridRogue.Game.Graphics
 open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Input
 
 type LevelState = { level: int }
 
@@ -9,17 +10,25 @@ type GameState =
         | MenuState
         | LevelState of LevelState
 
-let initialGameState = LevelState({ level = 1 })
+let initialGameState = MenuState
 
 let updateState (state: GameState) =
-    state
+    match state with
+        | MenuState ->
+            match Keyboard.GetState().IsKeyDown(Keys.Enter) with
+                | true ->
+                    LevelState({ level = 1})
+                | false ->
+                    state
+        | LevelState levelState ->
+            state
 
 let drawState (state: GameState) (graphics: GraphicsState) =
     let batch = graphics.batch
     batch.Begin()
     match state with
         | MenuState ->
-            ()
+            batch.DrawString(graphics.font, "Press Enter to start a new game", Vector2(0.0f, 300.0f), Color.White)
         | LevelState state ->
             batch.DrawString(graphics.font, (sprintf "Level %i" state.level), Vector2(0.0f, 0.0f), Color.White)
     batch.End()
