88d9cb3c91392442ea24a9004d1ff951401660f1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-07 22:50:14 +0100

    Make blocks options

--- HybridRogue.Game/JumpAndRun.fs
+++ HybridRogue.Game/JumpAndRun.fs
@@ -4,10 +4,10 @@ open Microsoft.Xna.Framework
 
 type Block = { tileType: int }
 
-type Map = { size: Point; blocks: Block array }
+type Map = { size: Point; blocks: Block option array }
 
 type Level = { map: Map }
 
 let defaultLevel = 
-    let blocks = [| for i in 0..25 -> { tileType = (i % 5) + 10 } |]
+    let blocks = [| for i in 0..25 -> Some({ tileType = (i % 5) + 10 }) |]
     { map = { size = Point(5, 5); blocks = blocks }}
