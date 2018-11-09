b030c125d54f603824dd0509b2a7faea98a66f39
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-09 20:04:52 +0100

    Dumb noise-based level generation

--- HybridRogue.Game/JumpAndRun.fs
+++ HybridRogue.Game/JumpAndRun.fs
@@ -1,6 +1,7 @@
 ﻿module HybridRogue.Game.JumpAndRun
 
 open Microsoft.Xna.Framework
+open OpenSimplexNoise
 
 type Block = { tileType: int; coordinates: int * int }
 
@@ -10,13 +11,31 @@ type Level = { map: Map; startingPoint: Point }
 
 let tileSize = 16
 
+type LevelParams = { size: Point; seed: int64 }
+
+let generateLevel (param: LevelParams) =
+    let mapSize = param.size.X * param.size.Y
+    let blocks: Block option array = [| for i in 0..(mapSize - 1) -> None |]
+    let noise = OpenSimplexNoise(param.seed)
+    let variation = float(param.size.Y / 2)
+    for x = 0 to param.size.X - 1  do
+        let nv = abs(int(noise.Evaluate(float(x), 0.0) * variation))
+        let start = (param.size.Y - 2) - nv
+        for y = start to param.size.Y - 1 do
+            let index = (y * param.size.X) + x
+            let block = { tileType = 54; coordinates = (x, y) }
+            Array.set blocks index (Some(block))
+
+    { size = param.size; blocks = blocks }
+
 let defaultLevel = 
-    let blocks = [| for i in 0..25 -> Some({ tileType = (i % 5) + 10; coordinates = (i % 5, i / 5) }) |]
-    { map = { size = Point(5, 5); blocks = blocks }; startingPoint = Point(0, 300) }
+    let map = generateLevel ({ size = Point(100, 40); seed = 12L })
+    { map = map; startingPoint = Point(0, 300) }
 
 let mapIteri (iter: int -> int -> Block option -> unit) (map: Map) =
     for i = 0 to (Seq.length map.blocks) - 1 do
         let x = i % map.size.X
         let y = i / map.size.Y
         let block = Seq.item i map.blocks
-        do iter x y block
+        do iter x y block    
+
