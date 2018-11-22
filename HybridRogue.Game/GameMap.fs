37a2148433ccc029bc1b76f21e06654c0d6c00cd
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-11-22 18:23:19 +0100

    Add new game map structures in the hope of improving the code structure

---
+++ HybridRogue.Game/GameMap.fs
@@ -0,0 +1,51 @@
+﻿module HybridRogue.Game.GameMap
+
+open Microsoft.Xna.Framework
+
+type Enemy = { health: int; radius: float32; position: Vector2; size: Vector2 }
+
+type Block = { tileType: int; position: Vector2; size: int; color: Color }
+
+type GameObject =
+    | Block of Block
+    | Enemy of Enemy
+    | NoObject
+
+type GameObjectStore = ResizeArray<GameObject>
+
+let createGameObjectStore (initialSize: int): GameObjectStore =
+    ResizeArray(initialSize)
+    
+let iterObjects (store: GameObjectStore) (iter: GameObject -> unit) =
+    for obj in store do iter obj
+
+let addObject (store: GameObjectStore) (obj: GameObject) =
+    store.Add(obj)
+    store.Count - 1
+
+let removeObject (store: GameObjectStore) (obj: GameObject) =
+    let index = store.IndexOf(obj)
+    store.Item index <- NoObject
+
+type BlockMap = { size: Point; tileSize: int; blocks: int array; store: GameObjectStore; box: Rectangle }
+
+let createBlockMap (size: Point) =
+    Array.create (size.X * size.Y) -1
+
+let retrieveObject (store: GameObjectStore) (index: int) = store.Item index
+
+let getBlock (map: BlockMap) (tileX: int) (tileY: int) =
+    let index = (map.size.X * tileY) + tileX
+    let item = Array.item index map.blocks
+    if item = -1 then
+        NoObject
+    else
+        retrieveObject map.store index
+
+let blockAt (map: BlockMap) (pos: Vector2) =
+    if map.box.Contains(pos) then
+        let tileX = int(pos.X) / map.tileSize
+        let tileY = int(pos.Y) / map.tileSize
+        (tileX, tileY, getBlock map tileX tileY)
+    else
+        (-1, -1, NoObject)
